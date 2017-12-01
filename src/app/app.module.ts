a734612e9f7ff64050537df1bc25e99382fb035a
max <maximediaznexton@gmail.com>
2017-12-01 22:58:40 +0100

    ajout des dossiers modules et components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
+    
   ],
   imports: [
     BrowserModule,

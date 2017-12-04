a6300ae8225e31f8ee3013f708681189b8dd017e
max <maximediaznexton@gmail.com>
2017-12-04 16:25:13 +0100

    ajout de bootstrap-3-grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css','../../node_modules/bootstrap-3-grid/bootstrap-3-grid.css']
 })
 export class AppComponent {
   title = 'app works';

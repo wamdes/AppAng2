b9349f5b7b669eace711a1e983037736eb60bcb1
caloudinou <pascalou@gmail.com>
2018-01-05 14:16:16 +0100

    other

--- src/app/modules/catalog/components/slideshow/slideshow.component.ts
+++ src/app/modules/catalog/components/slideshow/slideshow.component.ts
@@ -8,7 +8,12 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 export class SlideshowComponent implements OnInit, OnDestroy {
   count;
   public diapos = {
-    contents : [{img: '/assets/img/slideshow-mobile-01.jpg', desc: 'lalala1'}, {img: '/assets/img/slideshow-mobile-02.jpg', desc: 'lalala2'}, {img: '/assets/img/slideshow-mobile-03.jpg', desc: 'lalala3'}, {img: '/assets/img/slideshow-mobile-04.jpg', desc: 'lalala4'}],
+    contents : [
+      {img: '/assets/img/layer-1.png', title: 'TITRE 1 SUR UNE', subtitle: 'OU PLUSIEURS LIGNES', desc: 'Ce mois, partez a la decouverte ds classiques du roman noir...'},
+      {img: '/assets/img/slider-2.jpg', title: 'TITRE 2 SUR UNE', subtitle: 'OU PLUSIEURS LIGNES', desc: 'choisissez l\'un de nos voyages rares actuellement à l\'affiche...'},
+      {img: '/assets/img/train-3.jpg', title: 'TITRE 3 SUR UNE', subtitle: 'OU PLUSIEURS LIGNES', desc: 'Ce mois, partez a la decouverte ds classiques du roman noir...'},
+      {img: '/assets/img/slider-2.jpg', title: 'TITRE 4 SUR UNE', subtitle: 'OU PLUSIEURS LIGNES', desc: 'Toutes vos envies seront désormais comblées avec nos meilleurs offres...'}
+    ],
     num:[ 1, 2, 3, 4]
   };
 
@@ -16,10 +21,12 @@ export class SlideshowComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     (<any>window).slide = () => {
-      this.count = 0;
+      this.count = 1;
       setInterval(() => {
-        if (this.count === (this.diapos.contents.length - 1)){
-          this.count = 0;
+        console.log(this.diapos.contents.length - 1);
+        if (this.count === (this.diapos.contents.length - 1))
+        {
+          this.count = 1;
         }else {
           ++this.count;
         }
